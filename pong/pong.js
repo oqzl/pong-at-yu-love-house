7298263d289169de6bb42555c65ff8fdf11f23e8
kishikaw <kishikaw@yahoo-corp.jp>
2021-03-20 16:04:30 +0900

    mod

--- pong/pong.js
+++ pong/pong.js
@@ -67,13 +67,16 @@ var Paddle = {
 
 var Game = {
   initialize: function () {
+    let c = document.querySelector("canvas");
+    console.log(c);
+
     this.canvas = document.querySelector("canvas");
     this.context = this.canvas.getContext("2d");
 
     // this.canvas.width = 1400;
     // this.canvas.height = 1000;
-    this.canvas.width = this.canvas.clientWidth;
-    this.canvas.height = this.canvas.clientHeight;
+    this.canvas.width = this.canvas.clientWidth * 2;
+    this.canvas.height = this.canvas.clientHeight * 2;
 
     this.canvas.style.width = this.canvas.width / 2 + "px";
     this.canvas.style.height = this.canvas.height / 2 + "px";
