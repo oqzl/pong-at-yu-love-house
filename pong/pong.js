b93f8e917f018e4aaa256d2784cdc6b77f45c2f6
kishikaw <kishikaw@yahoo-corp.jp>
2021-03-20 17:14:21 +0900

    mod

--- pong/pong.js
+++ pong/pong.js
@@ -79,8 +79,8 @@ var Game = {
     this.context = this.canvas.getContext("2d");
 
     // let ratio = window.devicePixelRatio;
-    this.canvas.width = 1400;
-    this.canvas.height = 1000;
+    // this.canvas.width = 1400;
+    // this.canvas.height = 1000;
     // let w = document.querySelector("#wrapper");
     // this.canvas.width = w.offsetWidth / ratio / 2;
     // this.canvas.height = w.offsetHeight / ratio / 2;
