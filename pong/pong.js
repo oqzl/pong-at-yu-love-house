199c067a1e405cf0c03fcdb0a60be5ed5b339fb7
kishikaw <kishikaw@yahoo-corp.jp>
2021-03-21 11:41:39 +0900

    mod

--- pong/pong.js
+++ pong/pong.js
@@ -167,11 +167,11 @@ var Game = {
     MOTION.deg = (Math.atan(a.z / a.x) / Math.PI) * 180;
     document.getElementById("debug").innerHTML = MOTION.deg;
     if (MOTION.deg > MOTION_Z_FROM + 10) {
-      MOTION_UP = true;
-      MOTION_DOWN = false;
-    } else if (MOTION.deg < MOTION_Z_FROM - 10) {
       MOTION_UP = false;
       MOTION_DOWN = true;
+    } else if (MOTION.deg < MOTION_Z_FROM - 10) {
+      MOTION_UP = true;
+      MOTION_DOWN = false;
     } else {
       MOTION_UP = false;
       MOTION_DOWN = false;
